2e0472e884aa2b09c2fe61be6de26eab87356ff9
ayman <a@ay-man.com>
2024-07-24 20:08:39 +0000

    modules/dynamodb: main.tf (3/3)

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,108 @@
+resource "aws_dynamodb_table" "main" {
+  name           = var.table_name
+  billing_mode   = var.billing_mode
+  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+  hash_key       = var.hash_key
+  range_key      = var.range_key
+
+  dynamic "attribute" {
+    for_each = var.attributes
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_indexes
+    content {
+      name               = global_secondary_index.value.name
+      hash_key           = global_secondary_index.value.hash_key
+      range_key          = global_secondary_index.value.range_key
+      projection_type    = global_secondary_index.value.projection_type
+      non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
+      read_capacity      = var.billing_mode == "PROVISIONED" ? global_secondary_index.value.read_capacity : null
+      write_capacity     = var.billing_mode == "PROVISIONED" ? global_secondary_index.value.write_capacity : null
+    }
+  }
+
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_indexes
+    content {
+      name               = local_secondary_index.value.name
+      range_key          = local_secondary_index.value.range_key
+      projection_type    = local_secondary_index.value.projection_type
+      non_key_attributes = lookup(local_secondary_index.value, "non_key_attributes", null)
+    }
+  }
+
+  ttl {
+    attribute_name = var.ttl_attribute
+    enabled        = var.ttl_attribute != "" ? true : false
+  }
+
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_enabled ? var.stream_view_type : null
+
+  server_side_encryption {
+    enabled     = var.server_side_encryption_enabled
+    kms_key_arn = var.server_side_encryption_kms_key_arn
+  }
+
+  point_in_time_recovery {
+    enabled = var.point_in_time_recovery_enabled
+  }
+
+  tags = var.tags
+}
+
+resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
+  count              = var.billing_mode == "PROVISIONED" && var.autoscaling_enabled ? 1 : 0
+  max_capacity       = var.autoscaling_read_target.max_capacity
+  min_capacity       = var.autoscaling_read_target.min_capacity
+  resource_id        = "table/${aws_dynamodb_table.main.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
+  count              = var.billing_mode == "PROVISIONED" && var.autoscaling_enabled ? 1 : 0
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target[0].resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dynamodb_table_read_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target[0].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+    target_value = var.autoscaling_read_target.target_value
+  }
+}
+
+resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
+  count              = var.billing_mode == "PROVISIONED" && var.autoscaling_enabled ? 1 : 0
+  max_capacity       = var.autoscaling_write_target.max_capacity
+  min_capacity       = var.autoscaling_write_target.min_capacity
+  resource_id        = "table/${aws_dynamodb_table.main.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
+  count              = var.billing_mode == "PROVISIONED" && var.autoscaling_enabled ? 1 : 0
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target[0].resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target[0].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+    target_value = var.autoscaling_write_target.target_value
+  }
+}
