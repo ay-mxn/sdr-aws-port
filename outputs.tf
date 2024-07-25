fe4ba8ae6b2a2572fadb973d43f12b95c8574b3f
ayman <a@ay-man.com>
2024-07-25 17:30:51 +0000

    update source files

---
+++ outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = module.ecs_cluster.cluster_name
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = module.ecs_service.service_name
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = module.lambda.function_name
+}
+
+output "api_gateway_invoke_url" {
+  description = "The URL to invoke the API Gateway"
+  value       = module.api_gateway.invoke_url
+}
+
+output "dynamodb_table_name" {
+  description = "The name of the DynamoDB table"
+  value       = module.dynamodb.table_name
+}
+
+output "secrets_manager_secret_arn" {
+  description = "The ARN of the Secrets Manager secret"
+  value       = module.secrets_manager.secret_arn
+}
+
+output "cloudwatch_log_group_name" {
+  description = "The name of the CloudWatch log group"
+  value       = module.cloudwatch.log_group_name
+}
--- variables.tf
+++ variables.tf
@@ -1,16 +1,123 @@
-# variables.tf
-
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "The AWS region to deploy to"
+  type        = string
   default     = "us-west-2"
 }
 
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "sdr"
+}
+
 variable "environment" {
-  description = "Environment name (e.g., dev, test, prod)"
-  default     = "dev"
+  description = "The deployment environment (e.g., dev, staging, prod)"
+  type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "The CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
+
+variable "ecr_repository_url" {
+  description = "URL of the ECR repository containing the container image"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Port exposed by the container"
+  type        = number
+  default     = 80
+}
+
+variable "ecs_service_desired_count" {
+  description = "Desired number of tasks for the ECS service"
+  type        = number
+  default     = 2
+}
+
+variable "lambda_zip_file" {
+  description = "Path to the Lambda deployment package"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "Lambda function handler"
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "Lambda function runtime"
+  type        = string
+  default     = "nodejs14.x"
+}
+
+variable "lambda_environment_variables" {
+  description = "Environment variables for the Lambda function"
+  type        = map(string)
+  default     = {}
+}
+
+variable "dynamodb_hash_key" {
+  description = "The attribute to use as the hash key for the DynamoDB table"
+  type        = string
+  default     = "id"
+}
+
+variable "dynamodb_attributes" {
+  description = "List of attribute definitions for the DynamoDB table"
+  type = list(object({
+    name = string
+    type = string
+  }))
+  default = [
+    {
+      name = "id"
+      type = "S"
+    }
+  ]
+}
+
+variable "cloudwatch_alarms" {
+  description = "List of CloudWatch alarm configurations"
+  type = list(object({
+    name                = string
+    comparison_operator = string
+    evaluation_periods  = number
+    metric_name         = string
+    namespace           = string
+    period              = number
+    statistic           = string
+    threshold           = number
+    alarm_description   = string
+    alarm_actions       = list(string)
+    dimensions          = map(string)
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
 }
