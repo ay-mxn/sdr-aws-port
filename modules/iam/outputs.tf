bbe5835986bb2ac56a1f9759171a89636cea522c
ayman <a@ay-man.com>
2024-07-29 13:22:17 +0000

    modules/iam: outputs (3/4)

---
+++ modules/iam/outputs.tf
@@ -0,0 +1,9 @@
+output "ecs_task_execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  value       = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "lambda_execution_role_arn" {
+  description = "ARN of the Lambda execution role"
+  value       = aws_iam_role.lambda_execution_role.arn
+}
